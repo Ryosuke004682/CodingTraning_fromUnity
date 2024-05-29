ea24f0204bf4b54fe65ac9cff7f17bc7d79e3868
ryousuke <83821881+Ryosuke004682@users.noreply.github.com>
2024-05-30 00:22:03 +0900

    If文で、オブジェクトの形状を変化させるコードの追加

--- CodingTraning/Assets/Script/Traning_C#_Script/Test_2.cs
+++ CodingTraning/Assets/Script/Traning_C#_Script/Test_2.cs
@@ -1,18 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Test_2 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    MeshFilter meshFilter;
+
+    private void Start()
     {
-        
+        meshFilter = this.GetComponent<MeshFilter>();
+
+        if (meshFilter == null)
+        {
+            meshFilter = this.gameObject.AddComponent<MeshFilter>();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update() => ChangeMesh();
+
+
+    /// <summary>
+    /// マウスのクリックでオブジェクトの形状を変えるコード
+    /// </summary>
+    protected void ChangeMesh()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            meshFilter.mesh = CreateSphereMesh();// 新しい球のメッシュを作成
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            meshFilter.mesh = MeshSquareCreate();// 新しい四角のメッシュを作成
+        }
+    }
+
+    /// <summary>
+    /// 球のメッシュを作成する。
+    /// </summary>
+    /// <returns></returns>
+    private Mesh CreateSphereMesh()
+    {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        Mesh sphereMesh = sphere.GetComponent<MeshFilter>().mesh;
+        Destroy(sphere);
+        return sphereMesh;
+    }
+
+    /// <summary>
+    /// 四角形のメッシュを作成する。
+    /// </summary>
+    /// <returns></returns>
+    private Mesh MeshSquareCreate()
+    {
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        Mesh cubeMesh = cube.GetComponent<MeshFilter>().mesh;
+        Destroy(cube);
+        return cubeMesh;
     }
 }
